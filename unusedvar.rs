905aa2b28d5d0a7a2e0ffaa751c2204e35585586
Ansh Goyal <anshgoyal0604@gmail.com>
2024-04-13 22:36:42 +0530

    update

---
+++ unusedvar.rs
@@ -0,0 +1,10 @@
+// in this we se how to remove warning of unused variable 
+// There are 2 ways 
+// 1. by using '_' before the variable name 
+// 2.  by using ' #[allow(unused_variables)]' at the Top of the program
+#[allow(unused_variables)]
+fn main() {
+    let x = 5;
+    let _y =6;
+}
+
