7e3823e1d04f5ac7d6a2639c24d72e0e7bab47f5
Ansh Goyal <anshgoyal0604@gmail.com>
2024-04-21 23:48:09 +0530

    loop

---
+++ loops_rust.rs
@@ -0,0 +1,15 @@
+// Loops in rust
+// There are two types of loops in Rust - loop and for. Loop is a general purpose loop that can be used to repeatedly execute a block
+// There are two types of loops available in Rust - loop and for. The loop keyword is used to create an infinite loop, which will
+// There are two types of loops available in Rust - loop and for. Loop is a general purpose loop that can be used to repeatedly execute
+// There are two types of loops in Rust - loop and for. Loop is a general purpose loop that can be used to repeatedly execute a
+// For loop
+fn main() {
+    let nums = [1,  2, 3, 4, 5];
+    
+    for i in &nums {
+        println!("{}", i);
+    }
+}
+
+  
